f25ecd5fe2f84715a9e077616b6254e6db54aad7
David Pierce <davidprc3@gmail.com>
2022-10-01 16:15:14 -0400

    Updated speech bubbles

--- Assets/Scripts/BubbleBehavior.cs
+++ Assets/Scripts/BubbleBehavior.cs
@@ -11,12 +11,15 @@ public class BubbleBehavior : NPC
     [SerializeField] private TextMeshProUGUI bubble_dialogue;
     [SerializeField] private Image image_displayed;
     [SerializeField] private int sprite_index = 0;
+    [SerializeField] private Collider2D text_collider;
+    [SerializeField] private GameObject target;
+
+    private float journeyLength;
 
     // Start is called before the first frame update
     void Start()
     {
         image_displayed.alphaHitTestMinimumThreshold = 0.5f;
-        TextDelaySeconds = 1/(wpm / 60f)/avg_word_len;
         dialogue_string = bubble_dialogue.text;
         bubble_dialogue.text = "";
         sprite_index = Random.Range(0, bubble_sprites.Count - 1);
@@ -27,15 +30,41 @@ public class BubbleBehavior : NPC
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        moveTowardsTarget();
     }
 
     public void DisplayText()
     {
         StartCoroutine(TypeDialogue(dialogue_string));
     }
+
+    public void setTarget(GameObject target_temp)
+    {
+        target = target_temp;
+        journeyLength = Vector3.Distance(transform.position, cam.WorldToScreenPoint(target.transform.position));
+    }
+
+    private void moveTowardsTarget()
+    {
+        //Vector3 forceDirection = transform.position - cam.WorldToScreenPoint(target.transform.position);
+        //text_collider.attachedRigidbody.AddForce(forceDirection * target_pull_force * Time.fixedDeltaTime);
+
+        // Distance moved equals elapsed time times speed..
+        float distCovered = (Time.time - startTime) * bubble_speed;
+
+        // Fraction of journey completed equals current distance divided by total distance.
+        float fractionOfJourney = distCovered / journeyLength;
+
+        // Set our position as a fraction of the distance between the markers.
+        transform.position = Vector3.Lerp(transform.position, cam.WorldToScreenPoint(target.transform.position), fractionOfJourney);
+        transform.localScale = Vector3.Lerp(Vector3.zero, 1.5f*Vector3.one, fractionOfJourney*2);
+        // Debug.Log("distCovered: " + distCovered);
+        // Debug.Log("fractionOfJourney: " + fractionOfJourney);
+        // Debug.Log("JourneyLength: " + journeyLength);
+    }
+
 
     IEnumerator TypeDialogue (string dialogue)
     {
--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -7,15 +7,29 @@ public class NPC : MonoBehaviour
 
     [SerializeField] protected float wpm = 200;
     [SerializeField] protected float avg_word_len = 4.7f;
-    protected float TextDelaySeconds = 0;
+    [SerializeField] protected float target_pull_force = 1f;
+    [SerializeField] protected float bubble_speed = 3f;
     [SerializeField] protected float SpriteDelay = 1f;
+    protected float TextDelaySeconds = 0;
+    protected float startTime = 0f;
+    protected Vector3 startPositionCamSpace;
+
     [SerializeField] protected List<Sprite> bubble_sprites;
+    [SerializeField] protected Camera cam;
+    [SerializeField] private List<BubbleBehavior> bubble_behaviors;
+    // Targets for the speech bubbles to gravitate towards
+    // should exist to the left and right of the npc.
+    // There should be one target per speech bubble.
+    [SerializeField] private List<GameObject> targets;
+    [SerializeField] protected GameObject mouth_obj;
 
 
-    // Start is called before the first frame update
-    void Start()
+
+    void Awake()
     {
-        
+        TextDelaySeconds = 1 / (wpm / 60f) / avg_word_len;
+        initializeData();
+        beginTalking();
     }
 
     // Update is called once per frame
@@ -23,4 +37,41 @@ public class NPC : MonoBehaviour
     {
         
     }
+
+    private void initializeData()
+    {
+        startTime = Time.time;
+        startPositionCamSpace = cam.WorldToScreenPoint(mouth_obj.transform.position);
+        randomizeList(targets);
+    }
+
+    private void beginTalking()
+    {
+        setInitialBubblePositions();
+    }
+
+
+    private void setInitialBubblePositions()
+    {
+        int i = 0;
+        foreach (BubbleBehavior b in bubble_behaviors)
+        {
+            b.gameObject.transform.position = cam.WorldToScreenPoint(mouth_obj.transform.position);
+            b.setTarget(targets[i]);
+            i = i < bubble_sprites.Count - 1 ? i + 1 : 0;
+        }
+    }
+
+    private void randomizeList<T>(List<T> list)
+    {
+        for (int i = 0; i < list.Count; i++)
+        {
+            T temp = list[i];
+            int randomIndex = Random.Range(i, list.Count);
+            list[i] = list[randomIndex];
+            list[randomIndex] = temp;
+        }
+    }
+
+
 }
