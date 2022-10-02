329de85f187e08869e41e3880a4e44326e065881
David Pierce <davidprc3@gmail.com>
2022-10-02 17:42:07 -0400

    Updated items to pile sprites. Corrected hitboxes

--- Assets/Scripts/ButtonHitboxFix.cs
+++ Assets/Scripts/ButtonHitboxFix.cs
@@ -5,10 +5,12 @@ using UnityEngine.UI;
 
 public class ButtonHitboxFix : MonoBehaviour
 {
+    private Image image_displayed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        image_displayed = GetComponent<Image>();
+        image_displayed.alphaHitTestMinimumThreshold = 0.5f;
     }
 
 }
